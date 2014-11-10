8ed01a51887fd9a77ed7346185d855dfb5029100
alejandroxp90 <alejandro_xp90@hotmail.com>
2014-11-09 23:35:17 -0600

    Ciclo For 2

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Form6_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer
+        Dim b As Integer
+        Dim c As Integer
+        a = Val(TextBox1.Text)
+        b = 1
+        For c = 1 To a
+            b = b * c
+            TextBox2.Text = (b)
+        Next
+    End Sub
+End Class
